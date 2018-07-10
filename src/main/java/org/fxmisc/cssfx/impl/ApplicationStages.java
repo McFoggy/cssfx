39d30859f3036b579e91411cacd0ddd96a0aaf1d
François Martin <fracoisma@hotmail.com>
2018-07-10 15:46:05 +0200

    Replace call to com.sun.javafx.stage.StageHelper.getStages() (non-existent in Java 9) with call to Window.getWindows()

--- src/main/java/org/fxmisc/cssfx/impl/ApplicationStages.java
+++ src/main/java/org/fxmisc/cssfx/impl/ApplicationStages.java
@@ -21,24 +21,27 @@ package org.fxmisc.cssfx.impl;
  */
 
 
+import static java.util.stream.Collectors.toList;
 import static org.fxmisc.cssfx.impl.log.CSSFXLogger.logger;
 
-import java.lang.reflect.Method;
-
+import java.util.stream.Collectors;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class ApplicationStages {
     public static ObservableList<Stage> monitoredStages(Stage ...restrictedTo) {
         try {
-            Class<?> sh = Class.forName("com.sun.javafx.stage.StageHelper");
-            Method m = sh.getMethod("getStages");
-            ObservableList<Stage> stages = (ObservableList<Stage>) m.invoke(null, new Object[0]);
-            logger(ApplicationStages.class).debug("successfully retrieved JavaFX stages from com.sun.javafx.stage.StageHelper");
+            ObservableList<Stage> stages = Window.getWindows().stream()
+                .map(Stage.class::cast)
+                .collect(
+                    Collectors.collectingAndThen(toList(), FXCollections::observableArrayList)
+                );
+            logger(ApplicationStages.class).debug("successfully retrieved JavaFX stages by calling javafx.stage.Window.getWindows()");
             return stages;
         } catch (Exception e) {
-            logger(ApplicationStages.class).error("cannot observe stages changes by calling com.sun.javafx.stage.StageHelper.getStages()", e);
+            logger(ApplicationStages.class).error("cannot observe stages changes by calling javafx.stage.Window.getWindows()", e);
         }
         return FXCollections.emptyObservableList();
     }
